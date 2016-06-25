c29335278d48d8b5e90dcf5cac0cf9545fbc331d
Stepan Gatilov <stgatilov@gmail.com>
2016-06-25 22:52:09 +0600

    More detailed comments in utils header.

--- ArrayWithHash_Utils.h
+++ ArrayWithHash_Utils.h
@@ -15,37 +15,47 @@
 	#define AWH_NOINLINE 
 #endif
 
-//for testing purposes and for method AssertCorrectness
-#define AWH_ASSERT_ALWAYS(expr) { \
-	if (!(expr)) { \
-		fprintf(stderr, "Assertion failed: %s in %s:%s\n", #expr, __FILE__, __LINE__); \
-		assert(#expr != 0); \
-	} \
-}
-
-template<class typeA, class typeB> bool follows(typeA a, typeB b) {
-	return !a || b;
-}
-
-//to support the case when C++11 is not available
-#ifndef AWH_NO_CPP11
-	#define AWH_MOVE(x) std::move(x)
-#else
-	#define AWH_MOVE(x) (x)
+#ifdef AWH_TESTING	//only for testing purposes
+	//assert that is never thrown away
+	#define AWH_ASSERT_ALWAYS(expr) { \
+		if (!(expr)) { \
+			fprintf(stderr, "Assertion failed: %s in %s:%s\n", #expr, __FILE__, __LINE__); \
+			assert(#expr != 0); \
+		} \
+	}
+	//boolean implication operator
+	template<class typeA, class typeB> bool follows(typeA a, typeB b) {
+		return !a || b;
+	}
 #endif
 
 
-//default slow implementations
-template<class Size> Size log2size(Size sz) {
-	Size res = 0;
-	while (res < 8 * sizeof(Size) && (Size(1) << res) <= sz)
-		res++;
-	return res;
+//support the cases when C++11 is not available
+#ifndef AWH_NO_CPP11
+	#define AWH_MOVE(x) std::move(x)
+#else
+	#define AWH_MOVE(x) x
+#endif
+
+
+//================================================================
+//integer logarithm routines are defined below
+
+//returns minimal K such that X fits into array of 2^K size
+//used extensively for hash elements during relocations
+template<class Size> Size log2size(Size x) {
+	Size k = 0;
+	while (k < 8 * sizeof(Size) && (Size(1) << k) <= x)
+		k++;
+	return k;
 }
-template<class Size> static AWH_INLINE Size log2up(Size sz) {
-	return sz == 0 ? 0 : log2size(sz - 1);
+//returns binary logarithm of X rounded up
+//used rarely (does not affect performance)
+template<class Size> static AWH_INLINE Size log2up(Size x) {
+	return x == 0 ? 0 : log2size(x - 1);
 }
-//fast implementations
+
+//compiler-specific fast implementations relying in BSR x86 instruction
 #if _MSC_VER >= 1600
 	static AWH_INLINE uint32_t log2size(uint32_t sz) {
 		unsigned long pos;
@@ -77,6 +87,7 @@ template<class Size> static AWH_INLINE Size log2up(Size sz) {
 	}
 	#endif
 #endif
+//fast version for x64 integers on x32 platform
 #if (_MSC_VER >= 1600 || __GNUC__) && !(defined(_M_X64) || defined(__amd64__))
 	static AWH_INLINE uint64_t log2size(uint64_t sz) {
 		union {
@@ -90,5 +101,8 @@ template<class Size> static AWH_INLINE Size log2up(Size sz) {
 	}
 #endif
 
+//call 32-bit version for integers of smaller size
 static AWH_INLINE uint16_t log2size(uint16_t sz) { return log2size(uint32_t(sz)); }
 static AWH_INLINE uint8_t log2size(uint8_t sz) { return log2size(uint32_t(sz)); }
+
+//================================================================
