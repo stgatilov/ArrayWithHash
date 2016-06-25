c24af5da285cc41ad42d46e7640bced9b11a97a5
Stepan Gatilov <stgatilov@gmail.com>
2016-06-25 18:25:19 +0600

    Enabled controlled inlining (GCC).

--- c_mingw.bat
+++ c_mingw.bat
@@ -1,3 +1,3 @@
-g++ TestsMain.cpp CorrectnessTests.cpp PerformanceTests.cpp timer.c -O2 -std=c++11 -D NDEBUG -o TestsMain_mingw.exe
+g++ TestsMain.cpp CorrectnessTests.cpp PerformanceTests.cpp timer.c -O2 -std=c++11 -D NDEBUG -D AWH_CONTROL_INLINING -o TestsMain_mingw.exe
 if errorlevel 1 exit
-g++ PerformanceTests.cpp -S -O2 -std=c++11 -D NDEBUG
+g++ PerformanceTests.cpp -S -O2 -std=c++11 -D NDEBUG -D AWH_CONTROL_INLINING
