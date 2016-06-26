326c059045f819b6216de9566475a056d897ba47
Stepan Gatilov <stgatilov@gmail.com>
2016-06-26 13:43:54 +0600

    Finished commenting the main header.

--- ArrayWithHash.h
+++ ArrayWithHash.h
@@ -343,31 +343,46 @@ private:
 		hashFill = hashCount;
 	}
 
+	//reallocate array and hash table parts with given sizes
+	//called internally in two cases: automatic reallocation, Reserve method
 	AWH_NOINLINE void Reallocate(Size newArraySize, Size newHashSize) {
 		assert(newArraySize >= arraySize && newHashSize >= hashSize);
 
 		if (newHashSize == hashSize) {
 			if (newArraySize == arraySize)
+				//both sizes has not changed: just clean the hash
 				RelocateHashInPlace<false>(newArraySize);
 			else
+				//only array size has changed: clean the hash and filter new array elements
 				RelocateHashInPlace<true>(newArraySize);
 		}
 		else {
 			if (newArraySize == arraySize)
+				//only hash size has changed: reallocate and clean it
 				RelocateHashToNew<false>(newHashSize, newArraySize);
 			else
+				//both sizes have changed: reallocate them, clean the hash and filter new array elements
 				RelocateHashToNew<true>(newHashSize, newArraySize);
 		}
 	}
 
+	//======================================================================
+	//Each simple public method can operate either on array or on hash table.
+	//Here the methods are implemented for the case they operate on hash part.
+	//
+	//Since operations on hash part are not so critical for performance,
+	//they are marked as NOINLINE (when AWH_CONTROL_INLINING is enabled).
 
 	AWH_NOINLINE Value HashGet(Key key) const {
+		//check for null required: FindCellXXX hangs otherwise
 		if (hashSize == 0)
 			return ValueTraits::GetEmpty();
+		//find cell with the key (or first empty cell if not present)
 		Size cell = FindCellKeyOrEmpty(key);
 		return hashKeys[cell] == EMPTY_KEY ? ValueTraits::GetEmpty() : hashValues[cell];
 	}
 
+	//(almost the same as HashGet)
 	AWH_NOINLINE Value *HashGetPtr(Key key) const {
 		if (hashSize == 0)
 			return NULL;
@@ -377,28 +392,46 @@ private:
 
 	AWH_NOINLINE Value *HashSet(Key key, Value value) {
 		if (IsHashFull(hashFill, hashSize)) {
+			//fill ratio of hash part is at its allowed maximum
+			//reallocation may be necessary to finish the operation
 			AdaptSizes(key);
+			//perform the operation after reallocation
+			//note: we cannot just do HashGet here,
+			//because the element may now go into the array part
 			return Set(key, AWH_MOVE(value));
 		}
+		//find cell with the key (or first empty cell if not present)
+		//note: hash table cannot be null, since IsHashFull returns true in such case
 		Size cell = FindCellKeyOrEmpty(key);
+		//check if the key is new
 		bool newElement = (hashKeys[cell] == EMPTY_KEY);
+		//update fill/count counters
 		hashFill += newElement;
 		hashCount += newElement;
+		//save the key
 		hashKeys[cell] = key;
+		//if the element is already present, we have to destroy its value
+		//otherwise destination is already dead (i.e. not constructed)
 		if (!newElement)
 			hashValues[cell].~Value();
+		//move-construct the value in hash table from parameter
 		new (&hashValues[cell]) Value(AWH_MOVE(value));
+		//return pointer to the updated value
 		return &hashValues[cell];
 	}
 
+	//(very similar to HashSet)
 	AWH_NOINLINE Value *HashSetIfNew(Key key, Value value) {
 		if (IsHashFull(hashFill, hashSize)) {
+			//fill ratio is capped: reallocate and proceed as usual
 			AdaptSizes(key);
 			return SetIfNew(key, AWH_MOVE(value));
 		}
 		Size cell = FindCellKeyOrEmpty(key);
+		//if the element is not new, then simply return pointer to it
 		if (hashKeys[cell] != EMPTY_KEY)
 			return &hashValues[cell];
+		//the element is new: insert as usual
 		hashFill++;
 		hashCount++;
 		hashKeys[cell] = key;
@@ -407,24 +440,35 @@ private:
 	}
 
 	AWH_NOINLINE void HashRemove(Key key) {
+		//check for null required: FindCellXXX hangs otherwise
 		if (hashSize == 0)
 			return;
+		//find cell with the key (or first empty cell if not present)
 		Size cell = FindCellKeyOrEmpty(key);
+		//if key was not found, then do nothing
 		if (hashKeys[cell] == EMPTY_KEY)
 			return;
+		//mark cell of hash table as REMOVED
 		hashKeys[cell] = REMOVED_KEY;
 		hashCount--;
+		//destroy value of the removed element
 		hashValues[cell].~Value();
 	}
 
 	AWH_NOINLINE void HashRemovePtr(Value *ptr) {
+		//determine cell index
 		size_t cell = ptr - &hashValues[0];
 		assert(hashKeys[cell] != EMPTY_KEY && hashKeys[cell] != REMOVED_KEY);
+		//mark cell of hash table as REMOVED
 		hashKeys[cell] = REMOVED_KEY;
 		hashCount--;
+		//destroy value of the removed element
 		hashValues[cell].~Value();
 	}
 
+	//======================================================================
+
+	//initialize all members of this object to empty state
 	AWH_INLINE void Flush() {
 		arraySize = 0;
 		hashSize = 0;
@@ -435,6 +479,7 @@ private:
 		hashValues = NULL;
 		hashKeys = NULL;
 	}
+	//copy all members of this object from source object
 	AWH_INLINE void RelocateFrom(const ArrayWithHash &iSource) {
 		arraySize = iSource.arraySize;
 		hashSize = iSource.hashSize;
@@ -445,24 +490,31 @@ private:
 		hashValues = iSource.hashValues;
 		hashKeys = iSource.hashKeys;
 	}
+	//call destructor for all the values still alive in hash table part
+	//used for whole-object clearing
 	AWH_INLINE void DestroyAllHashValues() {
 		for (Size i = 0; i < hashSize; i++)
 			if (hashKeys[i] != EMPTY_KEY && hashKeys[i] != REMOVED_KEY)
 				hashValues[i].~Value();
 	}
 
-	//non-copyable
+	//note: ArrayWithHash is non-copyable
+	//on the other hand, it is movable and swappable (both in O(1))
 	ArrayWithHash (const ArrayWithHash &iSource);
 	void operator= (const ArrayWithHash &iSource);
 
 public:
 	ArrayWithHash() {
+		//set this to empty state
 		Flush();
 	}
 	AWH_NOINLINE ~ArrayWithHash() {
+		//call destructor for all values in array part
 		for (Size i = 0; i < arraySize; i++)
 			arrayValues[i].~Value();
+		//call destructor for all alive values in hash table part
 		DestroyAllHashValues();
+		//free all buffers
 		DeallocateBuffer<Value>(arrayValues);
 		DeallocateBuffer<Value>(hashValues);
 		DeallocateBuffer<Key>(hashKeys);
@@ -470,6 +522,7 @@ public:
 
 #ifndef AWH_NO_CPP11
 	//container is easily movable in C++11
+	//note: source object is reset to empty state
 	ArrayWithHash(ArrayWithHash &&iSource) {
 		RelocateFrom(iSource);
 		iSource.Flush();
@@ -480,8 +533,9 @@ public:
 	}
 #endif
 
-	//fast O(1) swap between containers
+	//fast O(1) swap of this object and another one
 	void Swap(ArrayWithHash &other) {
+		//implemented memberwise
 		std::swap(arraySize, other.arraySize);
 		std::swap(arrayCount, other.arrayCount);
 		std::swap(hashSize, other.hashSize);
@@ -492,36 +546,45 @@ public:
 		std::swap(hashKeys, other.hashKeys);
 	}
 
-	//remove all elements without shrinking
+	//remove all elements from container without shrinking
+	//note: if you want to free resources, use the well-known swap hack:
+	//  container.Swap(ArrayWithHash<...>());
 	AWH_NOINLINE void Clear() {
+		//note: if array is already empty, no action is required
 		if (arraySize && arrayCount) {
+			//make all values EMPTY
 			for (Size i = 0; i < arraySize; i++)
 				arrayValues[i] = AWH_MOVE(ValueTraits::GetEmpty());
 		}
+		//note: if hash table is already empty, no action is required
 		if (hashSize && hashFill) {
+			//destroy all the alive values of valid elements
 			DestroyAllHashValues();
+			//make all cells empty
 			std::fill_n(hashKeys, hashSize, EMPTY_KEY);
 		}
+		//reset all element counters
 		arrayCount = hashCount = hashFill = 0;
 	}
 
+	//return number of elements currently inside
 	AWH_INLINE Size GetSize() const {
 		return arrayCount + hashCount;
 	}
 
-	//return value for given key
-	//or "empty" value if not present
-	//Note: requires Value type to be copyable
+	//return value for given key, or EMPTY value if the key is not present
+	//note: Value must be copyable, otherwise this method won't compile 
+	//you can use GetPtr in case of non-copyable values
 	AWH_INLINE Value Get(Key key) const {
 		assert(key != EMPTY_KEY && key != REMOVED_KEY);
 		if (InArray(key))
+			//note: non-present values are already in EMPTY state in the array part
 			return arrayValues[key];
 		else
 			return HashGet(key);
 	}
 
-	//returns pointer to the value for given key
-	//or NULL if not present
+	//return pointer to the value for a given key, or NULL if key is not present
 	AWH_INLINE Value *GetPtr(Key key) const {
 		assert(key != EMPTY_KEY && key != REMOVED_KEY);
 		if (InArray(key)) {
@@ -532,8 +595,8 @@ public:
 			return HashGetPtr(key);
 	}
 
-	//sets value associated with given key
-	//key is inserted if not present before
+	//set the value associated with the given key
+	//the key is inserted if not present before
 	//returns pointer to the updated/inserted value
 	AWH_INLINE Value *Set(Key key, Value value) {
 		assert(key != EMPTY_KEY && key != REMOVED_KEY);
@@ -549,7 +612,7 @@ public:
 	}
 
 	//if key is present, then returns pointer to it
-	//otherwise adds a new key with associated value, and returns NULL
+	//otherwise inserts a new key with associated value, and returns NULL
 	AWH_INLINE Value *SetIfNew(Key key, Value value) {
 		assert(key != EMPTY_KEY && key != REMOVED_KEY);
 		assert(!ValueTraits::IsEmpty(value));
@@ -562,7 +625,8 @@ public:
 			}
 			else
 				return &oldVal;
-/*			Value *pOldVal = &arrayValues[key];		//branchless version worth it?
+			//is branchless version worth it?
+/*			Value *pOldVal = &arrayValues[key];		
 			Value stored = *pOldVal;
 			bool empty = ValueTraits::IsEmpty(stored);
 			stored = (empty ? value : stored);
@@ -574,31 +638,34 @@ public:
 			return HashSetIfNew(key, AWH_MOVE(value));
 	}
 
-	//removes given key (if present)
+	//remove element with the given key (if present)
 	AWH_INLINE void Remove(Key key) {
 		assert(key != EMPTY_KEY && key != REMOVED_KEY);
 		if (InArray(key)) {
 			Value &val = arrayValues[key];
 			arrayCount -= !ValueTraits::IsEmpty(val);	//branchless
+			//note: value is reset to EMPTY state in the array part
 			val = ValueTraits::GetEmpty();
 		}
 		else
 			HashRemove(key);
 	}
 
-	//removes key specified by pointer to its value
+	//remove element specified by pointer to its value
 	AWH_INLINE void RemovePtr(Value *ptr) {
 		assert(ptr);
 		assert(!ValueTraits::IsEmpty(*ptr));
 		if (InArray(ptr)) {
 			arrayCount--;
+			//reset value to EMPTY state
 			*ptr = ValueTraits::GetEmpty();
 		}
 		else
 			HashRemovePtr(ptr);
 	}
 
-	//returns key for the given value
+	//get key for the given value pointer
+	//this method allows to use value pointers as iterators
 	AWH_INLINE Key KeyOf(Value *ptr) const {
 		assert(ptr);
 		if (InArray(ptr))
@@ -609,22 +676,32 @@ public:
 		}
 	}
 
-	//force to allocate memory for at least given amount of elements (separately for array and hash parts)
+	//force to reserve some memory for both array and hash table parts
+	//  arraySizeLB: lower bound on number of elements in the array part
+	//   hashSizeLB: lower bound on number of cells in the hash table part
+	//note: maximal number of elements that can fit into hash table is smaller than number of cells
+	//note: array and hash table must have power-of-two sizes, so you values would be rounded up
+	//if alwaysCleanHash is true, then hash table would be cleaned even if no reallocation is necessary
 	AWH_NOINLINE void Reserve(Size arraySizeLB, Size hashSizeLB, bool alwaysCleanHash = false) {
+		//note: both parts remain of zero size if possible
 		if (arraySizeLB || arraySize)
 			arraySizeLB = std::max(Size(Size(1) << log2up(arraySizeLB)), std::max(arraySize, (Size)ARRAY_MIN_SIZE));
 		if (hashSizeLB  ||  hashSize)
 			hashSizeLB  = std::max(Size(Size(1) << log2up( hashSizeLB)), std::max( hashSize, (Size) HASH_MIN_SIZE));
+		//not necessary to run reallocation of sizes do not change (unless forced to clean hash)
 		if (arraySizeLB == arraySize && hashSizeLB == hashSize && !alwaysCleanHash)
 			return;
+		//perform reallocation and related tasks
 		Reallocate(arraySizeLB, hashSizeLB);
 	}
 
-	//perform given action for all the elements in container
-	//callback is specified in format:
+	//perform given action for all the elements in this container
+	//callback is specified as a functor with signature:
 	//  bool action(Key key, Value &value);
-	//it must return: false to continue iteration, true to stop
+	//it must return: false to continue iteration, true to stop it
+	//order of traversal may be arbitrary in general
 	template<class Action> void ForEach(Action &action) const {
+		//note: array part is traversed first since it is faster
 		for (Size i = 0; i < arraySize; i++)
 			if (!ValueTraits::IsEmpty(arrayValues[i]))
 				if (action(Key(i), arrayValues[i]))
@@ -638,47 +715,66 @@ public:
 #ifdef AWH_TESTING
 	//internal method: checks all the invariants of the container
 	//it is not called from anywhere (except tests), and you should not call it too
+	//
+	//verbosity allows to regulate how slow and detailed the check is:
+	//  0: perform only O(1) checks
+	//  1: also perform O(N) fast checks
+	//  2: perform all checks, including creating a temporary std::set
 	AWH_NOINLINE bool AssertCorrectness(int verbosity = 2) const {
 		if (verbosity >= 0) {
-			//check array/hash sizes
+			//each part either is null (size = 0) or has size capped from below
 			AWH_ASSERT_ALWAYS(arraySize == 0 || arraySize >= ARRAY_MIN_SIZE);
 			AWH_ASSERT_ALWAYS( hashSize == 0 ||  hashSize >=  HASH_MIN_SIZE);
+			//each size is always power of two
 			AWH_ASSERT_ALWAYS((arraySize & (arraySize - 1)) == 0);
 			AWH_ASSERT_ALWAYS(( hashSize & ( hashSize - 1)) == 0);
-			//check buffers
+			//if part has zero size, then its buffers must be null
+			//otherwise, buffers must be non-null
 			AWH_ASSERT_ALWAYS(follows(arraySize == 0, !arrayValues));
 			AWH_ASSERT_ALWAYS(follows(arraySize != 0,  arrayValues));
 			AWH_ASSERT_ALWAYS(follows( hashSize == 0,  !hashValues && !hashKeys));
 			AWH_ASSERT_ALWAYS(follows( hashSize != 0,   hashValues &&  hashKeys));
-			//check hash fill ratio
+			//fill ratio in the hash table part must never exceed hard-coded cap
 			AWH_ASSERT_ALWAYS(hashFill <= HASH_MAX_FILL * hashSize);
 		}
+
 		if (verbosity >= 1) {
-			//iterate over array
+			//count real number of elements in the array part
 			Size trueArrayCount = 0;
 			for (Size i = 0; i < arraySize; i++) {
 				if (ValueTraits::IsEmpty(arrayValues[i]))
 					continue;
 				trueArrayCount++;
 			}
+			//check array count value
 			AWH_ASSERT_ALWAYS(arrayCount == trueArrayCount);
-			//iterate over hash table
+
+			//count number of valid elements and number of non-empty cells in hash
 			Size trueHashCount = 0, trueHashFill = 0;
 			for (Size i = 0; i < hashSize; i++) {
 				Key key = hashKeys[i];
+				//small keys must always be located in the array part
 				AWH_ASSERT_ALWAYS(Size(key) >= arraySize);
+
 				if (key != EMPTY_KEY)
 					trueHashFill++;
 				if (key != EMPTY_KEY && key != REMOVED_KEY) {
+					//cell is occupied by a valid element
 					trueHashCount++;
 					const Value &value = hashValues[i];
-					AWH_ASSERT_ALWAYS(!ValueTraits::IsEmpty(value));	//must be alive
-				} //hashValues[i] must be dead otherwise
+					//its value must be alive and non-empty
+					AWH_ASSERT_ALWAYS(!ValueTraits::IsEmpty(value));
+				}
+				else {
+					//hashValues[i] must be dead (i.e. not constructed) in this case
+				}
 			}
+			//check the evaluated counters
 			AWH_ASSERT_ALWAYS(hashCount == trueHashCount && hashFill == trueHashFill);
 		}
+
 		if (verbosity >= 2) {
-			//hash verbose check: keys are unique
+			//use STL container to check that all keys in the hash table are unique
 			std::set<Key> keys;
 			for (Size i = 0; i < hashSize; i++) {
 				Key key = hashKeys[i];
@@ -687,21 +783,27 @@ public:
 				AWH_ASSERT_ALWAYS(keys.count(key) == 0);
 				keys.insert(key);
 			}
-			//hash verbose check: key placed near hash function value
+			//check that each element in hash table is reachable
+			//from its base cell by linear probing
 			for (Size i = 0; i < hashSize; i++) {
 				Key key = hashKeys[i];
 				if (key == EMPTY_KEY || key == REMOVED_KEY)
 					continue;
+				//valid element, run a search of it in the hash table	
 				Size cell = FindCellKeyOrEmpty(key);
+				//the search must have sucessfully found the element
 				AWH_ASSERT_ALWAYS(hashKeys[cell] == key);
 			}
 		}
+
+		//return value only for convenience
 		return true;
 	}
 #endif
 };
 
 //make sure std::swap works via Swap method
+//theoretically, it is also ok to swap by default with three moves (unless C++11 is disabled)
 namespace std {
 	template<class Key, class Value, class KeyTraits, class ValueTraits>
 	AWH_INLINE void swap(
